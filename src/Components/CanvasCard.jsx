96fd91f4dcf7134a88b8991f511373c08c9a9668
Anas <anaselsheikh2004@gmail.com>
2025-07-26 23:24:29 +0300

    statistics page finished

--- src/Components/CanvasCard.jsx
+++ src/Components/CanvasCard.jsx
@@ -3,9 +3,9 @@ import "../../styles/canvasCard.css";
 export default function CanvasCard(props) {
   return (
     <>
-      <div className="canvasContainer d-flex flex-column mx-auto">
+      <div className="canvasContainer d-flex flex-column mx-auto w-100">
         <p className="text-center">{props.title}</p>
-        <div className="text-center mx-auto">{props.canvas}</div>
+        <div className="text-center mx-auto w-100">{props.canvas}</div>
       </div>
     </>
   );
