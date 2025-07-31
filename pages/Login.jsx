52cecf1c139417531107efe75ea53feb8e378c67
Anas <anaselsheikh2004@gmail.com>
2025-07-31 15:05:02 +0300

    Login and register are finished

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -42,7 +42,7 @@ export default function Login() {
     } catch (error) {
       await Swal.fire({
         title: "Login Failed",
-        text: error.message,
+        text: "something went wrong",
         icon: "error",
         confirmButtonText: "OK",
       });
